ebe22dc88aa4155c4bfe835280d24fa319a8ea3b
Tuag <igortuag@gmail.com>
2023-11-14 08:42:24 -0300

    check database url

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable not set')
+}
+
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: { filename: process.env.DATABASE_URL },
