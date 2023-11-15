fc233c80fcd80f67c97b55eb2f364a85f6a545ab
Tuag <igortuag@gmail.com>
2023-11-15 07:37:39 -0300

    add node env

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,6 +2,9 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
