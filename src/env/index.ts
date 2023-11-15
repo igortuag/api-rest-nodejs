ff24d038443c9f5268e991688a415438edda2990
Tuag <igortuag@gmail.com>
2023-11-15 07:40:29 -0300

    add safe parse to validation

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,4 +9,12 @@ const envSchema = z.object({
   PORT: z.number().default(3333),
 })
 
-export const env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.log('Invalid environment variables!', _env.error.format())
+
+  throw new Error('Invalid environment variables!')
+}
+
+export const env = _env.data
