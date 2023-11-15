f629e0e02c5930266f8ad016e1fec324335f5a84
Tuag <igortuag@gmail.com>
2023-11-15 07:35:46 -0300

    add validation to dot env

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,9 @@
-import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable not set')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite3',
-  connection: { filename: process.env.DATABASE_URL },
+  connection: { filename: env.DATABASE_URL },
   useNullAsDefault: true,
   migrations: { extension: 'ts', directory: './db/migrations' },
 }
