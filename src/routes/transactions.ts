8384000e63e894f88363eada4437ad8b2579b514
Tuag <igortuag@gmail.com>
2023-11-17 09:33:41 -0300

    add type to plugin

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,6 +1,7 @@
+import { FastifyInstance } from 'fastify'
 import knex from 'knex'
 
-export function transactionsRoutes(app) {
+export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
     const transactions = await knex('transactions')
       .where('amount', '>', 0)
