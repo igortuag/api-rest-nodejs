aff68fc0d81b0d920eb1b491408a54c7898182b2
Tuag <igortuag@gmail.com>
2023-11-18 07:25:46 -0300

    add client http to test routes

--- src/server.ts
+++ src/server.ts
@@ -13,5 +13,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log('Server listening on http://localhost:3000')
+    console.log(`Server listening on http://localhost:${env.PORT}`)
   })
