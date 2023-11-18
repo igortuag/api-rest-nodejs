dc62ebbb51cf30d14040fb05844116bf56df9d3b
Tuag <igortuag@gmail.com>
2023-11-18 07:12:21 -0300

    get title amount type from validate body

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,20 +5,14 @@ import knex from 'knex'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
-    // const { title, amount, type } = request.body
-
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     })
 
-    const body = createTransactionBodySchema.parse(request.body)
-
-    const transactions = await knex('transactions')
-      .where('amount', '>', 0)
-      .select('*')
-
-    return transactions
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
   })
 }
