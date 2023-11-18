6357cb48830a8789d09639ad75fdac2c9b89b9cb
Tuag <igortuag@gmail.com>
2023-11-18 07:08:55 -0300

    add route prefix

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 import knex from 'knex'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async (request, reply) => {
+  app.post('/', async (request, reply) => {
     const transactions = await knex('transactions')
       .where('amount', '>', 0)
       .select('*')
