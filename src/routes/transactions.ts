36215dedc78711d2df6af1230e4630f0b7088404
Tuag <igortuag@gmail.com>
2023-11-18 07:10:51 -0300

    add createTransactionBodySchema

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,8 +1,18 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
 import knex from 'knex'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
+    // const { title, amount, type } = request.body
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
     const transactions = await knex('transactions')
       .where('amount', '>', 0)
       .select('*')
