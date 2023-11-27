d5cd593346e8b87b9701b7ffa5ea408e7288321e
Tuag <igortuag@gmail.com>
2023-11-27 08:14:06 -0300

    add example of test

---
+++ src/test/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import { test } from 'vitest'
+
+test('the user should be able to create a transaction', async () => {
+  // do a http request to the server
+
+  const responseStatusCode = 200
+
+  expect(responseStatusCode).to.equal(200)
+})
