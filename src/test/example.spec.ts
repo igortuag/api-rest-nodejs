5beae1c6969c9b2b369d4229e5ca2f81fff15d26
Tuag <igortuag@gmail.com>
2023-11-28 08:53:20 -0300

    add example of test transaction

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -5,4 +5,11 @@ import { app } from '../app'
 
 test('the user should be able to create a transaction', async () => {
   await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'new transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
