dfdd597c023258173360366e8cbffe78ad63ae28
Tuag <igortuag@gmail.com>
2023-11-28 08:55:46 -0300

    add after all to close app

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -1,9 +1,13 @@
-import { test, beforeAll } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
 beforeAll(async () => {
   await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
 })
 
 test('the user should be able to create a transaction', async () => {
