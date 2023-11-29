2e63da82a543de89b6618553ab85e4b95c18502d
Tuag <igortuag@gmail.com>
2023-11-29 09:11:17 -0300

    add group of tests

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,22 +1,24 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { test, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('the user should be able to create a transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'new transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  test('the user should be able to create a transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
