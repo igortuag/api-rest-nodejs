69ddef67d4c5ce6ea281045bf47319f8acb4d557
Tuag <igortuag@gmail.com>
2023-12-02 05:52:22 -0300

    add test to check summary

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -71,4 +71,27 @@ describe('Transactions routes', () => {
       }),
     )
   })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'credit transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({ title: 'debit transaction', amount: 2000, type: 'debit' })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    expect(summaryResponse.body).toEqual(
+      expect.objectContaining({
+        amount: 3000,
+      }),
+    )
+  })
 })
